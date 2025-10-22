bc7a946dead8caad1f850a173ffea44ca907d75d
kimsungwon318 <sungwon.kim@narimoto.co.jp>
2025-10-22 14:15:53 +0900

    prisma docker add

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaClient } from 'generated/prisma';
 
 @Controller()
 export class AppController {
@@ -7,6 +8,13 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    const prisma = new PrismaClient();
+
+    prisma.test.create({
+      data: {
+        id: '1',
+      },
+    });
     return this.appService.getHello();
   }
 }
