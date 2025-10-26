1beae002251ed9e1521a31b81b260b57a8543c15
kimsungwon318 <sungwon.kim@narimoto.co.jp>
2025-10-26 14:07:58 +0900

    guard add

---
+++ backend/src/auth/strategies/access-token.strategy.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+type JwtPayload = {
+  sub: string;
+  username: string;
+};
+
+@Injectable()
+export class AccessTokenStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-access-token',
+) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.AUTH_SECRET,
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    return payload;
+  }
+}
